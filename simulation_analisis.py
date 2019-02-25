c40409e6bfea31ad16eca357c3d91f9412bc43ac
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2019-02-25 14:42:57 +0200

    Update simulation_analisis.py

--- simulation_analisis.py
+++ simulation_analisis.py
@@ -104,5 +104,5 @@ for x in parameters:
         CORRELATION.append([pearsonr(x[0],y[0]),x[1],y[1]])
         
 CORELATION_FILE=open('CORRELATION_FILE','w')
-for writer in CORRELATION.sort(key=x[0][1]):
+for writer in CORRELATION.sort(key=lambda x: x[0][1]):
     CORELATION_FILE.write(str(writer[0][0])+'\t'+str(writer[0][1])+'\t'+str(writer[1])+'\t'+str(writer[2])+'\n')
