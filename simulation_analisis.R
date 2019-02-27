f147b2a4d7ac8631ac6b7469f7f9c50821778c8e
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2019-02-27 18:04:00 +0200

    Update simulation_analisis.R

--- simulation_analisis.R
+++ simulation_analisis.R
@@ -4,25 +4,92 @@ class(density)
 
 b=rnorm(100,100,2)
 a=density(b,n=11)$y
-a
-myFiles <- list.files(pattern="COMUSTATS_*")            
-myfile1=read.csv(file='COMUSTATS_6',sep='\t',header=TRUE, row.names=NULL)
-head(myfile1)
 
-
-
-
-
-inputFile <- "COMUSTATS_6"
-con  <- file(inputFile, open = "r")
-
-dataList <- list()
-ecdfList <- list()
-
-while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
+for (i in 56:99){
+############################################################
+#PARAMETERS input  
   
-j=strsplit(oneLine,'\t')
-print(j)
+ParametersFile <- paste ("PARAMETERS_",i, sep = "", collapse = NULL)
+con  <- file(ComusFile, open = "r")
+
+ParametersList <- list()
+j=0
+while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) 
+{
+  
+  ParametersList[j] <- strsplit(oneLine,'\t')
+  j <- j + 1
+  
   
 } 
+close(con)  
+  
+  
+  
+  
+  
+  
+  
+  
+############################################################  
+#COMUStats input
+  
+ComusFile <- paste ("COMUSTATS_",i, sep = "", collapse = NULL)
+con  <- file(ComusFile, open = "r")
+
+ComusList <- list()
+j=0
+while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) 
+  {
+  
+  ComusList[j] <- strsplit(oneLine,'\t')
+  j <- j + 1
+  
+  
+  } 
+close(con)
+
+##########################################################
+#f3 input
+
+F3file <- paste ("f3FINAL_",i,".txt", sep= "", collapse = NULL)
+con  <- file(F3file, open = "r")
+
+F3list <- list()
+j=0
+while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) 
+{
+  
+  F3list[j] <- strsplit(oneLine,'\t')
+  j <- j + 1
+  
+  
+} 
+close(con)
+
+
+#########################################################
+#PCA clustering
+
+PCAfile <- paste ("PCA_CLUSTERING_",i, sep = "", collapse = NULL)
+con  <- file(PCAfile, open = "r")
+
+PCAlist <- list()
+j=0
+while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) 
+{
+  
+  PCAlist[j] <- strsplit(oneLine,'\t')
+  j <- j + 1
+  
+  
+} 
+close(con)
+
+
+
+
+
+
+}
 
