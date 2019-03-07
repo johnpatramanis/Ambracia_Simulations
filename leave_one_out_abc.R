fdce606c3097496a8e58a28ffc07aec48bbafeef
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2019-03-07 11:48:49 +0200

    Add files via upload

---
+++ leave_one_out_abc.R
@@ -0,0 +1,62 @@
+#plot(density(c(-20, rep(0,98), 20)), xlim = c(-4, 4))
+#class(density)
+
+
+
+
+setwd("C:/Users/John/Desktop/Ambracia Sims/")
+
+
+system('grep -vwE "NA" FOR_ABC > FOR_ABC_CLEAN')
+library(abc)
+library(Metrics)
+
+
+a <- read.table("FOR_ABC_CLEAN", h=F)
+dim(a)
+predicted=list()
+actual=list()
+
+
+for (j in 1:dim(a)[1]){
+  
+  
+
+
+
+# check dims
+
+params <- a[-j,2:14]   #leave one out
+stats <- a[-j,-(1:14)] # << ,<<
+test <- a[j,-(1:14)]
+test_params <-a[j,2:14]
+
+dim(params)
+dim(stats) # check dims to make sure
+
+
+#headers=read.table("LABELS_FOR_ABC", h=F,sep='\t')
+#head(headers)#check headers
+
+#names(params) <- headers[1,1:14]
+#names(stats) <- mynames.stats[1,-(1:14)]
+
+
+
+mysample <- 1:248
+
+  
+myabc <- abc(target=test[,mysample], param=params, sumstat=stats[,mysample], tol=0.1, method="ridge", hcorr=TRUE)
+
+sum=summary(myabc)
+predicted[j]=as.numeric(sum[4,4])
+actual[j]=as.numeric(test_params[4])
+
+}
+
+mae(actual, predicted)
+
+actual
+predicted
+
+trace(abc, edit=TRUE)
