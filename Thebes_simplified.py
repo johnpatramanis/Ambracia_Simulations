6b0f9acdf03026c93283eee581ea5edb4ea2b415
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2019-05-18 18:33:04 +0300

    Update Thebes_simplified.py

--- Thebes_simplified.py
+++ Thebes_simplified.py
@@ -9,6 +9,19 @@ import random
 import sys
 import os.path
 from multiprocessing import Process,Manager
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas import DataFrame
+from bokeh.io import output_notebook, show
+from bokeh.plotting import figure
+from bokeh.models import TapTool, CustomJS, ColumnDataSource
+from bokeh.models import HoverTool
+from bokeh.io import output_file
+from bokeh import colors
 
 
 start_time = time.time()
@@ -132,7 +145,7 @@ def SIMULATE(L,argument,samples,population_configurations,migration_matrix,demog
     dd.write_vcf(wow,2,str(j))
     wow.close()
     
-    population_labels= ["Boet1"]*int(N1/2) + ["Boet2"]*int(N2/2) + ["Boet3"]*int(N3/2)
+    population_labels= ["Boet1_"]*int(N1/2) + ["Boet2_"]*int(N2/2) + ["Boet3_"]*int(N3/2)
     d=0
     newlabels=[]
     for i in range(0,len(population_labels)):
@@ -163,3 +176,86 @@ SIMULATE(L,0,samples,population_configurations,migration_matrix,demographic_even
 elapsed_time_1 = time.time() - start_time        
     
 print('Step 1 : {} '.format(elapsed_time_1/60))        
+
+
+
+vcf=open('myvcf0.vcf')
+
+
+
+def get_colors(n):
+    colorz=[]
+    for k in range(0,n):
+        c1=round(random.random(), 3)
+        c2=round(random.random(), 3)
+        c3=round(random.random(), 3)
+        colorz.append((c1,c2,c3))
+    return colorz
+#####IIIINCOOOMPLEEEETE
+
+
+
+
+for line in vcf:
+    if line[0]=='#' and line[1]!='#':
+        population_labels=line.strip().split()[9:]
+        true_labels=[]
+        for k in population_labels:
+            familyname=k.split('_')[0]
+            true_labels.append(familyname)
+        genotypes=[[] for x in range(0,len(population_labels))]
+    if line[0]!='#':
+        line=line.strip().split()
+        counter=0
+        for j in line[9:]:
+            if j=='0|0':
+                genotypes[counter].append(0)
+            if j=='1|0' or j=='0|1':
+                genotypes[counter].append(1)
+            if j=='1|1':
+                genotypes[counter].append(2)
+            if j=='.|.':
+                genotypes[counter].append(9)
+            counter+=1
+
+print(len(genotypes),len(genotypes[0]))
+for wow in genotypes:
+    print(len(wow))            
+#TRANSFORM TO tSNE
+X = np.asarray(genotypes)    
+X_embedded = TSNE(n_components=2,learning_rate=10.0,n_iter=1000,perplexity=5.0).fit_transform(X)    
+print(X_embedded.shape)   
+
+
+
+PLOTING
+plt.figure(figsize=(100, 60))
+
+COLORPALLETE=get_colors(len(set(true_labels)))
+COLORZ_TO_LABELS={}
+
+uniquelabels=[x for x in set(true_labels)]
+for j in range(0,len(uniquelabels)):
+    COLORZ_TO_LABELS[uniquelabels[j]]=COLORPALLETE[j]
+
+colors=[ COLORZ_TO_LABELS[x] for x in true_labels]
+   
+plt.scatter([x[0] for x in X_embedded],[x[1] for x in X_embedded],label=true_labels,c=colors)
+plt.show()
+
+
+pca = PCA(n_components=2).fit_transform(genotypes)
+X=[]
+Y=[]
+for j in pca:
+    X.append(j[0])
+    Y.append(j[1])
+
+plt.figure(figsize=(100, 60))
+
+   
+plt.scatter(X,Y,label=population_labels,c=colors)
+plt.show()
+        
+
+
