95415311975ed7b879d1b16dfb6789b53f3599e6
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2019-06-13 12:38:06 +0300

    Add files via upload

--- plotting_predictions.py
+++ plotting_predictions.py
@@ -3,48 +3,43 @@ import matplotlib.pyplot as plt
 import math 
 
 rep=1
-file=open('PREDICTIONS_{}'.format(rep),'r')
-actual=[]
-predicted=[]
-for line in file:
-    line=line.strip().split()
-    actual.append(float(line[0]))
-    predicted.append(float(line[1]))
+for rep in range(0,14):
+    file=open('PREDICTIONS_{}'.format(rep),'r')
+    actual=[]
+    predicted=[]
+    for line in file:
+        line=line.strip().split()
+        actual.append(float(line[0]))
+        predicted.append(float(line[1]))
 
-print(len(actual),len(predicted))
-RMSE=[]
-MAPE=[]
+    print(len(actual),len(predicted))
+    RMSE=[]
+    MAPE=[]
 
-#Root Mean Squared Error 
-for k in range(0,len(actual)):
-    RMSE.append((actual[k]-predicted[k])**2)
+    #Root Mean Squared Error 
+    for k in range(0,len(actual)):
+        RMSE.append((actual[k]-predicted[k])**2)
 
-ROOT=math.sqrt((sum(RMSE))/len(actual))
-RMSE=ROOT/(max(actual)-min(actual))
-print('Root Mean Squared Error is {} '.format(RMSE))
+    ROOT=math.sqrt((sum(RMSE))/len(actual))
+    RMSE=ROOT/(max(actual)-min(actual))
+    print('The Root Mean Squared Error for parameter number {} is {} '.format(rep,RMSE))
 
-#Mean Absolute Percentage Error
+    #Mean Absolute Percentage Error
 
-for k in range(0,len(actual)):
-    top=(actual[k]-predicted[k])
-    bot=(actual[k]+predicted[k])/2
-    
-    MAPE.append(abs(top/bot) )
-MAPE=sum(MAPE)/len(MAPE)
+    for k in range(0,len(actual)):
+        top=(actual[k]-predicted[k])
+        bot=(actual[k]+predicted[k])/2
+        
+        MAPE.append(abs(top/bot) )
+    MAPE=sum(MAPE)/len(MAPE)
 
-print('Mean Absolute Percentage Error is {} '.format(MAPE))
+    print('The Mean Absolute Percentage Error for parameter number {} is {} '.format(rep,MAPE))
 
 
 
-font = {'family': 'serif',
-        'color':  'blue',
-        'weight': 'normal',
-        'size': 14,
-        }
-
-plt.figure()
-plt.xlabel('Prediction', fontdict=font)    
-plt.ylabel('Actual', fontdict=font)    
-plt.title('Parameter number {}'.format(rep), fontdict=font)    
-plt.plot(predicted,actual,'.', line)  
-plt.savefig('prediction_{}.png'.format(rep))
+    plt.figure()
+    plt.xlabel('Prediction', fontdict=font)    
+    plt.ylabel('Actual', fontdict=font)    
+    plt.title('Parameter number {}'.format(rep), fontdict=font)    
+    plt.plot(x=predicted,y=actual,'.', line)  
+    plt.savefig('prediction_{}.png'.format(rep))
